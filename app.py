36e5cd8a31749934b00539db066d009b763c0939
Poojahyalij2001 <poojahyalij.sknsit.it@gmail.com>
2025-06-18 19:08:04 +0530

    newly added

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "This is the demo of docker"
+    return "This is the demo of docker for internship"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
